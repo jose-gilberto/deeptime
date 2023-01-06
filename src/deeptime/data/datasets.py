9dac3123d711f6b7c7ffda76fab3ee28f994d464
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2023-01-06 17:12:29 -0300

    feat: add ucr dataset

--- src/deeptime/data/datasets.py
+++ src/deeptime/data/datasets.py
@@ -12,6 +12,7 @@ from typing import Tuple, Union
 
 import numpy as np
 import torch
+from sktime.datasets import load_UCR_UEA_dataset
 from torch.utils.data import Dataset
 
 
@@ -46,8 +47,67 @@ class BaseDataset(Dataset):
 
 class UCRDataset(BaseDataset):
 
-    def __init__(self, x: np.ndarray, y: np.ndarray = None) -> None:
-        raise NotImplementedError
+    def __init__(
+        self,
+        name: str,
+        labeled: bool = True,
+        split: str = 'train'
+    ) -> None:
+        x, y = UCRDataset.load_ucr_dataset(name=name, split=split)
+
+        if labeled is False:
+            y = None
+
+        super().__init__(x=x, y=y)
+
+    @staticmethod
+    def load_ucr_dataset(
+        name: str,
+        split: str = 'train',
+        verbose: bool = False
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """ Download and load an UCR Dataset from the sktime library.
+        The dataset will be transformed into a array-like format compatible
+        with the torch.Dataset implementation.
+
+        See:
+            - https://www.timeseriesclassification.com/dataset.php
+
+        Args:
+            - name (str): The dataset name from the timeseriesclassification
+                table. Compatible with the sktime method to load the data.
+            - split (str): The data split that comes from sktime lib, must be
+                equals to 'train' or 'test'. Defaults to 'train'.
+            - verbose (bool, optional): If verbose its True, then the method
+                will output some steps to provide some way of logging to the
+                user. Defaults to False.
+
+        Returns:
+            - (Tuple[ndarray, ndarray]): Returns
+                a tuple with four positions, each one represents respectively
+                x, y.
+        """
+        # TODO: implement some way of passing a logger object to the method
+        # and if verbose its true use that logger to output the messages
+        if verbose:
+            print(f'Loading {name}/{split} dataset from sktime lib...')
+
+        x, y = load_UCR_UEA_dataset(name=name, split='train')
+
+        # Since the features from the sktime are interpreted as a Series
+        # inside of a dataframe we have to manually convert them
+        y = np.array(y, dtype=np.int32)
+
+        # Convert the sktime Series like object into a ndarray
+        x_trasformed = np.array([
+            value[0].tolist() for value in x.values
+        ])
+
+        # Now X features have a shape like (n_instances, n_features)
+        # We need to convert them to a (n_instances, 1, n_features)
+        x = np.expand_dims(x_trasformed, axis=1)
+
+        return x, y
 
 
 class UEADataset(BaseDataset):
